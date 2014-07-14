9ef26b727f5f87e14cc56fba10beb9d7072b6cee
Eoin McCarthy <eoinmccarthy@fastmail.fm>
2014-07-14 23:16:41 +0100

    Tweaked the startup of multiple readers. Pooling doesn't work if I step through the readers sequentially and wait for each to complete.

--- src/file_echo_app.erl
+++ src/file_echo_app.erl
@@ -3,7 +3,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1, echo_file/2]).
+-export([start/2, stop/1, echo_file/2, echo_files/1]).
 
 %% ===================================================================
 %% Application callbacks
@@ -18,4 +18,20 @@ stop(_State) ->
 
 echo_file(Filename, Mode) ->
 	file_reader:echo_file(file_reader, Filename, Mode).
+
+echo_files(Mode) ->
+	{ok, NumReaders} = application:get_env(file_echo, num_readers),
+	lists:foreach(fun(Num) ->
+			ProcessName = list_to_atom("reader_" ++ integer_to_list(Num)),
+			spawn(fun() ->
+				file_reader:echo_file(ProcessName, integer_to_list(Num)++".txt", Mode)
+			end)
+		end,
+		lists:seq(1, NumReaders)
+	).
+
+
+
+
+
 
